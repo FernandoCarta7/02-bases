d07983b45c59c30e92b165ed9ca43db80ad0a767
Fernando <cartafernando@outlook.com>
2023-05-17 10:34:42 -0500

    Modulo DBZ

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MainPageComponent } from './pages/main-pages.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MainPageComponent
+  ],
+
+  exports:[
+    MainPageComponent
+  ],
+
   imports: [
-    CommonModule
+    CommonModule,
+
   ]
 })
 export class DbzModule { }
